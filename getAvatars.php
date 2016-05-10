e977a161dcdd278331cf43c08c3de905f91f1826
Sinead O'Rourke <sineadcreates@gmail.com>
2016-05-10 09:50:11 +0100

    update Game + judging pages

---
+++ getAvatars.php
@@ -0,0 +1,39 @@
+<?php
+	//Variables to hold permission details
+	$servername = "localhost";
+	$username = "cauPlayer";
+	$password = "password1";
+	$db = "caudatabase";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $db);
+
+	// Check connection
+	if ($conn->connect_error) {
+    	die("Database connection failed: " . $conn->connect_error);
+	}
+	
+	//Get user details from the request 
+   	$gameRef = $_REQUEST['gameRef'];
+	$fireID = $_REQUEST['fireUID'];
+
+	$sql="SELECT FirebaseID, Avatar FROM cauusers WHERE GameNumber = '".$gameRef."'";
+
+	$result = mysqli_query($conn,$sql); // Store the results of the SQL query in a variable
+
+	if ($result->num_rows > 0) { // Echo the results back to results.html in the xhttp object ;
+		while($row = $result->fetch_assoc()) {
+        	echo $row['FirebaseID'] . ",".$row['Username'] . ",".$row['Avatar'] . ",";
+    	}
+    }
+
+
+	/*if(mysqli_query($conn, $sql)) { // Check status of call (development purposes)
+    		echo "Added " . $sql . " to the database under UID: " . $fireUID;
+	} else {
+    		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}*/
+
+	$conn->close(); // Close the connection
+
+?>
